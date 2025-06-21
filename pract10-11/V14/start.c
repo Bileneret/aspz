c5401a111dd719b229fd23d4c3b63d1f18df4ba0
Bileneret <sashabelik085@gmail.com>
2025-06-21 09:23:08 +0300

    Add files via upload

---
+++ pract10-11/V14/start.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        execl("./print", "print", (char *)NULL);
+        perror("execl failed");
+        exit(EXIT_FAILURE);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            printf("Child exited with code %d\n", WEXITSTATUS(status));
+        }
+    }
+
+    return 0;
+}
